d20fd4d972ef577750628d67de509d48b98443f9
Rahul Salunkhe <rsrahulsalunkhe4@gmail.com>
2025-03-15 22:28:44 +0530

    .

--- src/components/centerRateCard/CenterRateCard.jsx
+++ src/components/centerRateCard/CenterRateCard.jsx
@@ -5,9 +5,9 @@ import banner2 from './../../assets/banner2.jpeg'
 const Price = (props) => {
   return (
     <div className='price-status'>
-        <span className='' style={{fontSize: '15px', fontWeight: 700, color: '#606060'}}>{props.status}</span>
-        <span style={{fontSize: '17px', fontWeight: 500}}>{props.price}</span>
-        <span style={{fontSize: '16px', fontWeight: 500, color: props.color}}>{props.change}</span>
+        <span className='' style={{fontSize: '14px', fontWeight: 700, color: '#606060'}}>{props.status}</span>
+        <span style={{fontSize: '16px', fontWeight: 500}}>{props.price}</span>
+        <span style={{fontSize: '14px', fontWeight: 500, color: props.color}}>{props.change}</span>
     </div>
   )
 }
@@ -15,20 +15,20 @@ const Price = (props) => {
 const CenterRateCard = () => {
   return (
     <div>
-    <div className='mb-2 bg-primary'>
+    <div className='mb-3 bg-primary'>
         <div className='p-3'>
             <div className='d-flex align-items-center'>
                 <span className='date'>27-09-2023</span>
                 <p className='status m-0 ms-auto'>STEADY TO FIRM</p>
             </div>
-            <div className='d-flex mt-1'>
+            <div className='d-flex mt-2'>
                 <div className='location'>
                     <p className='m-0'>DELHI</p>
                     <span>RJ Line</span>
                 </div>
                 <div className='price'>
                     <span className='price-range'>₹5100-5200</span>
-                    <span className='up-down'><span className='up-down1'>+50</span></span>
+                    <p className='up-down'><span className='up-down1 py-1'>+50</span></p>
                 </div>
             </div>
 
@@ -41,21 +41,21 @@ const CenterRateCard = () => {
             </div>
             
             <div className='key-statistics'>
-                <Price status='7 Days' price='5000-5100' change='▲812' color='#00AA08'/><div className="divider2"></div>
-                <Price status='14 Days' price='5000-5100' change='▲812' color='#EE0000'/><div className="divider2"></div>
-                <Price status='21 Days' price='5000-5100' change='▲812' color='#00AA08'/>
+                <Price status='7 Days' price='₹5000-5100' change='▲812' color='#00AA08'/><div className="divider2"></div>
+                <Price status='14 Days' price='₹5000-5100' change='▲812' color='#EE0000'/><div className="divider2"></div>
+                <Price status='21 Days' price='₹5000-5100' change='▲812' color='#00AA08'/>
             </div>
 
             <div className='summery'>
                 <ul className='ps-3 pt-2'>
-                    <li style={{color: '#27292A', fontWeight: 600, lineHeight: 1.15}} className='mb-2 p-0'>Trading Activity Very Dull in Chana and rate down</li>
-                    <li style={{color: '#27292A', fontWeight: 600, lineHeight: 1.15}} className='mb-2 p-0'>Trading Activity Very Dull in Chana and rate down</li>
+                    <li style={{color: '#27292A', fontWeight: 500, lineHeight: 1.15}} className='mb-2 p-0'>Trading Activity Very Dull in Chana and rate down</li>
+                    <li style={{color: '#27292A', fontWeight: 500, lineHeight: 1.15}} className='mb-2 p-0'>Trading Activity Very Dull in Chana and rate down</li>
                 </ul>
             </div>
             <p className='text-left mt-2' style={{color: '#1400FC', fontWeight: 600, fontSize: '14px'}}>M P AGRO OVERSEAS, DELHI</p>
         </div>
     </div>
-    <div className='w-100 px-3 pb-2'>
+    <div className='w-100 pb-3'>
             <img className='w-100 h-100 rounded' src={banner2} alt="" loading="lazy" />
         </div>
     </div>
