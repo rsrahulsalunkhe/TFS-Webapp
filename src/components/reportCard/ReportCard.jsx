d6285f8c8192b5fd2977359d833defc5ef22d10c
Rahul Salunkhe <rsrahulsalunkhe4@gmail.com>
2025-03-19 00:00:44 +0530

    reports obj bind

--- src/components/reportCard/ReportCard.jsx
+++ src/components/reportCard/ReportCard.jsx
@@ -2,33 +2,56 @@ import React from 'react'
 import banner from '../../assets/banner.jpeg'
 import './style.scss'
 
-const ReportCard = () => {
+const ReportCard = ({ report }) => {
+    console.log("Report Data:", report.content.img);
   return (
-    <div className='card'>
-      <div className='w-100'>
-        <img className='w-100 h-100' src={banner} alt="banner" loading='lazy' />
-      </div>
-      <div className='py-3 px-3'>
-        <span className='date'>27-09-2023</span>
-        <h5 className='fw-bold pt-2' style={{color: 'var(--blue-primary)'}}>Maize trade offer Aboti canvassing Jalgaon</h5>
-        <div className='bg-gray-200 py-2 px-3 rounded'>
-            <h6 className='fw-bold m-0' style={{fontSize: '18px'}}>Deals in Maize, Wheat, Moong, Chana Urad, Jowar, Kabuli</h6>
+    <div>
+        <div className='card'>
+            {report?.content?.img ? (
+                <div className='w-100'>
+                    <img className='w-100 h-100' src={report.content.img.i} alt="banner" loading='lazy' />
+                </div>
+            ) : null}
+            <div className='py-3 px-3'>
+                <span className='date'>{report.rec_date}</span>
+                <h5 className='fw-bold pt-2' style={{color: 'var(--blue-primary)'}}>{report.content.card.tit}</h5>
+                {report?.content?.card?.subtit ? (
+                    <div className='bg-gray-200 py-2 px-3 rounded'>
+                        <h6 className='fw-bold m-0' style={{fontSize: '18px'}}>{report.content.card.subtit}</h6>
+                    </div>
+                ) : null }
+
+                <div className='divider1 mt-3'>
+                    <div className='gradient-background1'></div>
+                    <div className='gradient-dot me-2'></div>
+                    <span>HIGHLIGHTS</span>
+                    <div className='gradient-dot ms-2'></div>
+                    <div className='gradient-background2'></div>
+                </div>
+
+                {report?.content?.card?.p ? (
+                    <ul style={{paddingLeft: '1.5rem', marginBottom: '0'}} className='mt-2'>
+                        {report.content.card.p.map((p) => (
+                            <li className='fw-bold pb-3' style={{lineHeight: 1.2}}>{p.t}</li>
+                        ))}
+                    </ul>
+                ) : null}
+
+                {report?.content?.card?.b ? (
+                    <div className='d-flex'>
+                        <i className='ms-auto' style={{color: 'var(--blue-secondary)', fontWeight: 500}}>{report.content.card.b} &gt;</i>
+                    </div>
+                ) : null }
+            </div>
         </div>
 
-        <div className='divider1 mt-3'>
-            <div className='gradient-background1'></div>
-            <div className='gradient-dot me-2'></div>
-            <span>HIGHLIGHTS</span>
-            <div className='gradient-dot ms-2'></div>
-            <div className='gradient-background2'></div>
-        </div>
-
-        <ul style={{paddingLeft: '1.5rem', marginBottom: '0'}} className='mt-2'>
-            <li className='fw-bold pb-3' style={{lineHeight: 1.2}}>Trading Activity Very Dull in Chana and rate down</li>
-            <li className='fw-bold pb-3' style={{lineHeight: 1.2}}>Trading Activity Very Dull in Chana and rate down</li>
-        </ul>
-        <i style={{color: 'var(--blue-secondary)'}}>Load More Reports &gt;</i>
-      </div>
+        {report?.content?.ban && report?.content?.ban[0]?.i ? (
+            <div className='w-100 mt-3'>
+                {report.content.ban.map((b) => (
+                    <img className='w-100 h-100' src={b.i} alt="" />
+                ))}
+            </div>
+        ) : null}
     </div>
   )
 }
