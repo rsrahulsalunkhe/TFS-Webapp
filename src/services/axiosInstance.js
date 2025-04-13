1b253aaa77c88f3f91e7eb438781c55054c38a76
Rahul Salunkhe <rsrahulsalunkhe4@gmail.com>
2025-04-13 11:23:38 +0530

    add .env file

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,10 +1,10 @@
 // src/services/axiosInstance.js
 import axios from "axios";
 
-const API_BASE_URL = "https://grainmarket.in/anaj-admin/Apiweb"; // Replace with your API
-const token = "7733aeef73f32ab344531979deb827a1adeffca62a04e1a3d6e5242eb3fd2c2761ad75c84cb807e09cfe20bed88c3df77d88250e410f2ea2b67c68e9"
-const user_uid = "01JNENTT93D37B6D371CVQJZW2"
-const id = 49
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const token = import.meta.env.VITE_API_TOKEN;
+const user_uid = import.meta.env.VITE_API_USER_UID;
+const id = import.meta.env.VITE_API_USER_ID;
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
@@ -16,12 +16,6 @@ const axiosInstance = axios.create({
 // Request Interceptor
 axiosInstance.interceptors.request.use(
   (config) => {
-    // const token = localStorage.getItem("authToken"); // Get token from storage
-    // const lang = localStorage.getItem("lang") || "en"; // Default language
-
-    // if (token) {
-    //   config.headers["Authorization"] = `Bearer ${token}`;
-    // }
     config.headers["Token"] = token;
     config.headers["User-Id"] = id;
     config.headers["User-Uid"] = user_uid;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://grainmarket.in/anaj-admin/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  }
 })
