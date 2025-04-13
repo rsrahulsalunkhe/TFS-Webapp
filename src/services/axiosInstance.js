d5f0236167afb1dfbeba867539e3c8550255772f
Rahul Salunkhe <rsrahulsalunkhe4@gmail.com>
2025-04-13 10:44:36 +0530

    .

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 // src/services/axiosInstance.js
 import axios from "axios";
 
-const API_BASE_URL = "http://grainmarket.in/anaj-admin/Apiweb"; // Replace with your API
+const API_BASE_URL = "https://grainmarket.in/anaj-admin/Apiweb"; // Replace with your API
 const token = "7733aeef73f32ab344531979deb827a1adeffca62a04e1a3d6e5242eb3fd2c2761ad75c84cb807e09cfe20bed88c3df77d88250e410f2ea2b67c68e9"
 const user_uid = "01JNENTT93D37B6D371CVQJZW2"
 const id = 49
