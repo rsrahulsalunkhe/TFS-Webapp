0a0fb0a85cc1a08c6a5fda2055e9ee706ec72a0f
Rahul Salunkhe <rsrahulsalunkhe4@gmail.com>
2025-04-16 22:58:58 +0530

    .

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,10 +1,20 @@
 // src/services/axiosInstance.js
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-const token = import.meta.env.VITE_API_TOKEN;
-const user_uid = import.meta.env.VITE_API_USER_UID;
-const id = import.meta.env.VITE_API_USER_ID;
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// const token = import.meta.env.VITE_API_TOKEN;
+// const user_uid = import.meta.env.VITE_API_USER_UID;
+// const id = import.meta.env.VITE_API_USER_ID;
+
+const API_BASE_URL = 'https://grainmarket.in/anaj-admin/Apiweb';
+const token = '7733aeef73f32ab344531979deb827a1adeffca62a04e1a3d6e5242eb3fd2c2761ad75c84cb807e09cfe20bed88c3df77d88250e410f2ea2b67c68e9'
+const user_uid = '01JNENTT93D37B6D371CVQJZW2'
+const id = 49;
+
+// VITE_API_BASE_URL=https://grainmarket.in/anaj-admin/Apiweb
+// VITE_API_TOKEN=7733aeef73f32ab344531979deb827a1adeffca62a04e1a3d6e5242eb3fd2c2761ad75c84cb807e09cfe20bed88c3df77d88250e410f2ea2b67c68e9
+// VITE_API_USER_UID=01JNENTT93D37B6D371CVQJZW2
+// VITE_API_USER_ID=49
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
