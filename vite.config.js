130ab68035ffdc720492a137135f58037a3f9e78
Rahul Salunkhe <rsrahulsalunkhe4@gmail.com>
2025-05-01 19:46:13 +0530

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), svgr(), VitePWA({ registerType: 'autoUpdate' })]
+// })
+
 export default defineConfig({
-  plugins: [react(), svgr()]
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'TFS',
+        short_name: 'TFS',
+        description: 'Grain Market',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
